5f9fd8a63fb51d24b2de349898f0893202f5cc5c
dquishpe <dquishpe@espol.edu.ec>
2024-12-11 12:15:02 -0500

    firestore

---
+++ src/app/services/provider.service.ts
@@ -0,0 +1,30 @@
+ /* Agregue la función inject */
+ import { Injectable, inject } from '@angular/core';
+
+ /* Importe los módulos de AngularFire */
+ import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+ import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProviderService {
+
+  constructor() { }
+  
+  /* Inyecte de dependencia AngularFire */
+  firestoreService = inject(Firestore);
+
+  /* Método para crear un documento en la colección */
+  createDocument(collectionName: string, data: any): Promise<any> {
+    const colRef = collection(this.firestoreService, collectionName);
+    return addDoc(colRef, data);
+  }
+
+  /* Método para leer una colección */
+  readCollection(collectionName: string): Observable<any[]> {
+    const colRef = collection(this.firestoreService, collectionName);
+    return collectionData(colRef, { idField: 'id' });
+  }
+  
+}
