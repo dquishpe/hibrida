b131b4c87e25e52d1d6f7a03a70a642efa9ec69a
randyRivera0 <rariberm@espol.edu.ec>
2025-01-05 12:19:59 -0500

    Created a new tab for Jose's version and removed old conflicting files

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { medkitOutline } from 'ionicons/icons';
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss'],
+  standalone: true,
+  imports: [
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+  ],
+})
+export class Tab4Page {
+  constructor() {
+    addIcons({ medkitOutline });
+  }
+}
