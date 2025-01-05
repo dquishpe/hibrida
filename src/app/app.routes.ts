ee1cdc5d91a199b7d028378c1274da023e2ce010
randyRivera0 <rariberm@espol.edu.ec>
2025-01-05 12:45:37 -0500

    fix: recreate tab4 folder to avoid merge conflicts with main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,8 @@ export const routes: Routes = [
     path: 'tab4',
     loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
   },
+  {
+    path: 'tab3',
+    loadComponent: () => import('./tab3/tab3.page').then( m => m.Tab3Page)
+  },
 ];
