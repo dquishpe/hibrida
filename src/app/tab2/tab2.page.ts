23549165ab729317aa265ba7a59d3081dc768dcd
randyRivera0 <rariberm@espol.edu.ec>
2025-01-19 10:37:42 -0500

    Enable chart to shift between weeks for improved date range navigation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonCard, IonBadge, IonCardHeader, IonCardTitle, IonCardContent, IonSelect, IonSelectOption, IonTextarea, IonButton, IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonCard,
+  IonBadge,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea,
+  IonButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Chart } from 'chart.js/auto';
 import { ProviderService } from '../services/provider.service';
 
@@ -10,34 +33,66 @@ import { ProviderService } from '../services/provider.service';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
-    IonSelect, IonSelectOption, IonTextarea, IonButton, IonBadge,
-    IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonSelect,
+    IonSelectOption,
+    IonTextarea,
+    IonButton,
+    IonBadge,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+  ],
 })
-
 export class Tab2Page {
   dataList: any[] = [];
   filteredDataList: any[] = [];
   chart: any;
+  now = new Date();
+  startOfWeek: Date = this.getStartOfWeek();
+  endOfWeek: Date = this.getEndOfWeek();
   collectionName = 'reviews';
-  constructor(private providerService: ProviderService) { }
+  averageScore: number = 0;
+  feedbackMessage: string = '';
+
+  constructor(private providerService: ProviderService) {}
+
   myForm: FormGroup = new FormGroup({
-    score: new FormControl("", Validators.required),
-    opinion: new FormControl("", Validators.required)
+    score: new FormControl('', Validators.required),
+    opinion: new FormControl('', Validators.required),
   });
+
   onSubmit() {
     const currentDate = this.getCurrentDate(); // Fecha actual
-    const existingEntry = this.dataList.find(datum => datum.date === currentDate);
+    const existingEntry = this.dataList.find(
+      (datum) => datum.date === currentDate
+    );
     if (existingEntry) {
-      alert('You have already submitted a response for today :) Please come back tomorrow!'); // Mensaje de advertencia
-      return; // Salir sin guardar
+      alert(
+        'You have already submitted a response for today :) Please come back tomorrow!'
+      );
+      return;
     }
     const formData = { ...this.myForm.value, date: currentDate };
-    this.providerService.createDocument(this.collectionName, formData).then(() => {
-      this.myForm.reset();
-      this.loadData();
-    });
+    this.providerService
+      .createDocument(this.collectionName, formData)
+      .then(() => {
+        this.myForm.reset();
+        this.loadData();
+      });
   }
+
   getCurrentDate(): string {
     const date = new Date();
     const day = String(date.getDate()).padStart(2, '0');
@@ -45,50 +100,50 @@ export class Tab2Page {
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   }
+
   ngOnInit() {
     this.loadData();
   }
-  averageScore: number = 0;
-  feedbackMessage: string = '';
+
   loadData() {
-    this.providerService.readCollection(this.collectionName).subscribe((data) => {
-      this.dataList = data;
-      this.dataList.sort((a, b) => {
-        const [dayA, monthA, yearA] = a.date.split('/').map(Number);
-        const [dayB, monthB, yearB] = b.date.split('/').map(Number);
-        const dateA = new Date(yearA, monthA - 1, dayA);
-        const dateB = new Date(yearB, monthB - 1, dayB);
-        return dateA.getTime() - dateB.getTime();
+    this.providerService
+      .readCollection(this.collectionName)
+      .subscribe((data) => {
+        this.dataList = data;
+        this.dataList.sort((a, b) => {
+          const [dayA, monthA, yearA] = a.date.split('/').map(Number);
+          const [dayB, monthB, yearB] = b.date.split('/').map(Number);
+          const dateA = new Date(yearA, monthA - 1, dayA);
+          const dateB = new Date(yearB, monthB - 1, dayB);
+          return dateA.getTime() - dateB.getTime();
+        });
+        this.filterCurrentWeekData(); // Filtra los datos de la semana actual
+        this.getChart();
+        this.calculateWeeklyAverage();
+        this.updateFeedbackMessage();
       });
-      this.filterCurrentWeekData();  // Filtra los datos de la semana actual
-      this.createChart();  
-      this.calculateWeeklyAverage();
-      this.updateFeedbackMessage();
-    });
   }
+
   filterCurrentWeekData() {
-    const now = new Date();
-    const startOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - (now.getDay() === 0 ? 6 : now.getDay() - 1));  // Lunes como primer día
-    const endOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() + (6 - (now.getDay() === 0 ? 6 : now.getDay() - 1)));  // Sábado como último día
-    
-    this.filteredDataList = this.dataList.filter(datum => {
+    this.filteredDataList = this.dataList.filter((datum) => {
       const [day, month, year] = datum.date.split('/').map(Number);
       const date = new Date(year, month - 1, day);
-      return date >= startOfWeek && date <= endOfWeek;  // Comparación del rango completo de la semana
+      return date >= this.startOfWeek && date <= this.endOfWeek;
     });
   }
-  
+
   calculateWeeklyAverage() {
-    const lastSevenEntries = this.dataList.slice(-7);  // Obtener los últimos 7 registros
-    const validScores = lastSevenEntries.map(datum => parseInt(datum.score, 10)).filter(score => !isNaN(score)); // Filtrar solo puntajes válidos
+    const lastSevenEntries = this.dataList.slice(-7); // Obtener los últimos 7 registros
+    const validScores = lastSevenEntries
+      .map((datum) => parseInt(datum.score, 10))
+      .filter((score) => !isNaN(score)); // Filtrar solo puntajes válidos
     if (validScores.length > 0) {
       const totalScore = validScores.reduce((sum, score) => sum + score, 0);
-      this.averageScore = totalScore / validScores.length;  // Calcular promedio basado en los últimos 7 registros
+      this.averageScore = totalScore / validScores.length; // Calcular promedio basado en los últimos 7 registros
     } else {
-      this.averageScore = 0;  // No hay datos
+      this.averageScore = 0; // No hay datos
     }
   }
-  
 
   updateFeedbackMessage() {
     const roundedAverage = this.averageScore.toFixed(2); // Redondea a 2 decimales
@@ -102,34 +157,37 @@ export class Tab2Page {
       this.feedbackMessage = `It is a difficult time, with an average of ${roundedAverage}. But don't worry, everything gets better with time❤️‍🩹`;
     }
   }
-  createChart() {
-    const recentData = this.dataList.slice(-7); // Los 7 últimos registros
-    const scores = recentData.map(datum => datum.score); // Valores de 'score' de los últimos 7
-    const labels = recentData.map(datum => datum.date);  // Fechas de los últimos 7 como etiquetas
-  
+
+  getChart() {
+    const recentData = this.filteredDataList.slice(-7); // Los 7 últimos registros
+    const scores = recentData.map((datum) => datum.score); // Valores de 'score' de los últimos 7
+    const labels = recentData.map((datum) => datum.date); // Fechas de los últimos 7 como etiquetas
+
     const canvas = document.getElementById('scoreChart') as HTMLCanvasElement;
     const ctx = canvas?.getContext('2d');
-    
+
     if (!ctx) {
       console.error('Failed to get canvas context.');
       return;
     }
-    
+
     if (this.chart) {
-      this.chart.destroy();  // Destruir gráfico previo si existe
+      this.chart.destroy(); // Destruir gráfico previo si existe
     }
-    
+
     this.chart = new Chart(ctx, {
       type: 'line',
       data: {
-        labels: labels,  // Mostrar fechas en el eje X
-        datasets: [{
-          label: 'Score Over Time',
-          data: scores,
-          borderColor: 'rgba(75, 192, 192, 1)',
-          backgroundColor: 'rgb(255, 255, 255)',
-          borderWidth: 2,
-        }]
+        labels: labels, // Mostrar fechas en el eje X
+        datasets: [
+          {
+            label: 'Score Over Time',
+            data: scores,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgb(255, 255, 255)',
+            borderWidth: 2,
+          },
+        ],
       },
       options: {
         responsive: true,
@@ -137,17 +195,17 @@ export class Tab2Page {
         scales: {
           x: {
             ticks: {
-              color: 'white'  // Color de las etiquetas del eje X
+              color: 'white', // Color de las etiquetas del eje X
             },
             title: {
               display: true,
               text: 'Date',
               color: 'white',
-            }
+            },
           },
           y: {
             ticks: {
-              color: 'white'  // Color de las etiquetas del eje X
+              color: 'white', // Color de las etiquetas del eje X
             },
             beginAtZero: false,
             min: 1,
@@ -155,19 +213,51 @@ export class Tab2Page {
               display: true,
               text: 'Score',
               color: 'white',
-            }
-          }
+            },
+          },
         },
         plugins: {
           legend: {
             labels: {
-              color: 'white'  // Color del texto de la leyenda
-            }
-          }
-        }
-      }
+              color: 'white', // Color del texto de la leyenda
+            },
+          },
+        },
+      },
     });
   }
-  
 
+  shiftToPreviousWeek() {
+    this.startOfWeek.setDate(this.startOfWeek.getDate() - 7);
+    this.endOfWeek.setDate(this.endOfWeek.getDate() - 7);
+    this.filterCurrentWeekData();
+    this.getChart();
+    this.calculateWeeklyAverage();
+    this.updateFeedbackMessage();
+  }
+  shiftToNextWeek() {
+    this.startOfWeek.setDate(this.startOfWeek.getDate() + 7);
+    this.endOfWeek.setDate(this.endOfWeek.getDate() + 7);
+    this.filterCurrentWeekData();
+    this.getChart();
+    this.calculateWeeklyAverage();
+    this.updateFeedbackMessage();
+  }
+
+  getStartOfWeek(): Date {
+    return new Date(
+      this.now.getFullYear(),
+      this.now.getMonth(),
+      this.now.getDate() - (this.now.getDay() === 0 ? 6 : this.now.getDay() - 1)
+    ); // Lunes como primer día
+  }
+
+  getEndOfWeek(): Date {
+    const endOfWeek = new Date(this.now); // Clone the current date
+    endOfWeek.setDate(
+      this.now.getDate() +
+        (6 - (this.now.getDay() === 0 ? 6 : this.now.getDay() - 1))
+    ); // Set the end of the week (Sunday)
+    return endOfWeek;
+  }
 }
